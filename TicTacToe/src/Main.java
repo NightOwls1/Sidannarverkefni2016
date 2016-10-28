055ac9a18070cca14865239b3930e4b654b1d620
Kiddi <kiddinn95@gmail.com>
2016-10-28 14:22:50 +0000

    for synchronization purposes

---
+++ TicTacToe/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	
+	public static void main(String[] args){
+		UnitTest ut = new UnitTest();
+		System.out.println(ut.testMain());
+		
+/*		TicTacToe game = new TicTacToe();
+		try {
+			game.multiPlayerPlay();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+*/
+	}
+}
